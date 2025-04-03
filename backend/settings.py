38363773fd77719124219c25a6ea4559eca19b49
DCML-Lilia <liliatou44@gmail.com>
2025-04-03 16:08:28 +0200

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y^@f&z(*-zt5ns$ef2(!dal-a4*%ye=o#ycz6n363r&vl51dpr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '10.0.2.2','192.168.1.11']
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 
