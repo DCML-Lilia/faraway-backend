9124183eccf0d3a9bb16b1d13733c5a19f6c714c
DCML-Lilia <liliatou44@gmail.com>
2025-04-03 18:22:45 +0200

    Set DEBUG=False and configured ALLOWED_HOSTS

--- backend/settings.py
+++ backend/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y^@f&z(*-zt5ns$ef2(!dal-a4*%ye=o#ycz6n363r&vl51dpr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ["faraway-backend.onrender.com"]
+
 
 
 
