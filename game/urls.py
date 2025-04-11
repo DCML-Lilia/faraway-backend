535e94a1f73a97a9bc906a7e8ab19b373509c10b
DCML-Lilia <liliatou44@gmail.com>
2025-04-11 20:02:29 +0200

    Modifs des fichiers urls pour que la communication entre flutter et le backend se fasse n.2

--- game/urls.py
+++ game/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path, include
-from .views import PhotoUploadView
+# game/urls.py
+
+from django.urls import path
+from .views import PhotoUploadView  # ou tout autre view que tu veux exposer
 
 urlpatterns = [
-    path('upload/', PhotoUploadView.as_view()),  # <- nécessaire pour Flutter
+    path('upload/', PhotoUploadView.as_view()),  # donc /api/upload/
+    path('game/', PhotoUploadView.as_view()),    # donc /api/game/
 ]
